7bedd72cc7b1bc32d57cabefb90013240e86e643
Gabriel Inacio <inaciogs03@gmail.com>
2021-08-05 16:05:44 -0300

    objeto cobrinha criado

--- script.js
+++ script.js
@@ -2,10 +2,27 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //Renderiza em 2D
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 //definindo forma
 function criarBG(){
     context.fillStyle = "lightgreen"; //background
     context.fillRect(0, 0, 16 * box, 16 * box); //desenhando nosso retangulo: x, y, altura, largura;
 }
+
+//criando o objeto(cobra) - Array de coordenadas
+function criarCobrinha(){
+    for(i=0; i<snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
+
+
